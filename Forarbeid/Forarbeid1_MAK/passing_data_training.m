340550102dc1739560dd02e4e9368f5b2a0093ac
Martinakraft99 <martiakr@stud.ntnu.no>
2021-02-18 11:46:05 +0100

    Script to taks 2

---
+++ Forarbeid/Forarbeid1_MAK/passing_data_training.m
@@ -0,0 +1,11 @@
+
+x_travel_ref = timeseries([0,2,4,0],[0,1,2,3]);
+
+x_elev_ref = timeseries([0,1,1,1],[0,1,2,3]);
+
+
+simin = [x_travel_ref; x_elev_ref];
+
+load(data_recorded);
+
+
---
+++ Forarbeid/Forarbeid2_MAK/quadProg.asv
@@ -0,0 +1,9 @@
+syms lambda r p dp p_c
+
+x = [labda;r;p;dp];
+u = p_c;
+
+%creating z
+
+
+
