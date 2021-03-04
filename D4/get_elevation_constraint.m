da2b110fb5251fc850283e2e5f912959ebd8f98b
Martinakraft99 <martiakr@stud.ntnu.no>
2021-03-04 12:33:12 +0100

    Day 4 attempted, not running

--- D4/get_elevation_constraint.m
+++ D4/get_elevation_constraint.m
@@ -1,6 +1,8 @@
-function [c_ie] = get_elevation_constraint(x,alpha,beta,lambda_t)
+function [c_ie] = get_elevation_constraint(z,alpha,beta,lambda_t,N,mx)
 %Yield the constraint defined by eq22
-c_ie = alpha*exp(-beta*(x(1)-lambda_t)^2)-x(5);
-
+c_ie = ones(N,1);
+for i=0:N-1
+    c_ie(i) = alpha*exp(-beta*(z(1+i*mx)-lambda_t)^2)-z(5+i*mx);
 end
 
+c_ie
